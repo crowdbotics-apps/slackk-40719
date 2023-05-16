7f14f790f11e6f1232e567c55c9c78fee8690eec
Crowdbotics <team@crowdbotics.com>
2023-05-16 05:14:23 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/slackk_40719/MainActivity.java
@@ -0,0 +1,15 @@
+package com.slackk_40719;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "slackk_40719";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "slackk-40719",
+  url: "https://slackk-40719.botics.co",
+  api: "https://slackk-40719.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
